9f0573888933155433fcf970f7d2606b6d5fbe84
Youying <15221352470@163.com>
2016-05-26 10:46:35 +0800

    Update controllersSpec.js
    step-2

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -71,3 +71,16 @@ describe('PhoneCat controllers', function() {
     });
   });
 });
+
+describe('PhoneCat controllers', function() {
+
+  describe('PhoneListCtrl', function(){
+
+    it('should create "phones" model with 3 phones', function() {
+      var scope = {},
+      ctrl = new PhoneListCtrl(scope);
+
+      expect(scope.phones.length).toBe(3);
+    });
+  });
+});
