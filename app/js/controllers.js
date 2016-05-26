20fd78a27c1c9327d4cfca43ed68d2bc5b1e53ef
Youying <15221352470@163.com>
2016-05-26 09:06:45 +0800

    Update controllers.js

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -24,11 +24,11 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Ph
   function PhoneListCtrl($scope) {
     $scope.phones=[
       {"name":"Nexus S",
-        "snippet":"Fast just got faster with Nexus S."},
+        "snippet": "Fast just got faster with Nexus S."},
       {"name":"Motorola XOOM™ with Wi-Fi",
-        "snippet":"The Next, Next Generation tablet."},
+        "snippet": "The Next, Next Generation tablet."},
       {"name":"MOTOROLA XOOM™",
-        "snippet":"The Next, Next Generation tablet." }
+        "snippet": "The Next, Next Generation tablet."}
       ];
     
   }
