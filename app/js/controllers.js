31b647e1f74b53fdd2bea77ba94a332da295e49b
Youying <15221352470@163.com>
2016-05-26 09:27:13 +0800

    Update controllers.js

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,18 +8,6 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
   function($scope, Phone) {
     $scope.phones = Phone.query();
     $scope.orderProp = 'age';
-     function PhoneListCtrl($scope) {
-    $scope.phone=[
-      {"name":"Nexus S",
-        "snippet": "Fast just got faster with Nexus S."},
-      {"name":"Motorola XOOM™ with Wi-Fi",
-        "snippet": "The Next, Next Generation tablet."},
-      {"name":"MOTOROLA XOOM™",
-        "snippet": "The Next, Next Generation tablet."}
-      ];
-    
-  }
- 
   }]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
@@ -32,6 +20,18 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Ph
       $scope.mainImageUrl = imageUrl;
     };
   }]);
+  
+    function PhoneListCtrl($scope) {
+    $scope.phones = [
+    {"name": "Nexus S",
+     "snippet": "Fast just got faster with Nexus S."},
+    {"name": "Motorola XOOM™ with Wi-Fi",
+     "snippet": "The Next, Next Generation tablet."},
+    {"name": "MOTOROLA XOOM™",
+     "snippet": "The Next, Next Generation tablet."}
+    ];
+    }
+  
   
   
   
